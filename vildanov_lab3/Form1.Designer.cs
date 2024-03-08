bd99f5dcc51593334c5f9b608274cc3889a679d6
artem-vildanov <tema2000art@gmail.com>
2024-03-08 22:14:13 +0700

    init

---
+++ vildanov_lab3/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace vildanov_lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Lbl1 = new System.Windows.Forms.Label();
+            this.Ed1 = new System.Windows.Forms.NumericUpDown();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.CellsFieldInForm = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Ed1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CellsFieldInForm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Lbl1
+            // 
+            this.Lbl1.AutoSize = true;
+            this.Lbl1.Font = new System.Drawing.Font("Microsoft Tai Le", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl1.Location = new System.Drawing.Point(32, 90);
+            this.Lbl1.Name = "Lbl1";
+            this.Lbl1.Size = new System.Drawing.Size(60, 30);
+            this.Lbl1.TabIndex = 0;
+            this.Lbl1.Text = "Rule";
+            // 
+            // Ed1
+            // 
+            this.Ed1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Ed1.Location = new System.Drawing.Point(134, 86);
+            this.Ed1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.Ed1.Name = "Ed1";
+            this.Ed1.Size = new System.Drawing.Size(120, 34);
+            this.Ed1.TabIndex = 1;
+            this.Ed1.Value = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            this.Ed1.ValueChanged += new System.EventHandler(this.edRule_ValueChanged);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BtnStart.Location = new System.Drawing.Point(37, 179);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(217, 91);
+            this.BtnStart.TabIndex = 2;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BtnStop.Location = new System.Drawing.Point(37, 306);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(217, 91);
+            this.BtnStop.TabIndex = 3;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // CellsFieldInForm
+            // 
+            this.CellsFieldInForm.AllowUserToAddRows = false;
+            this.CellsFieldInForm.AllowUserToDeleteRows = false;
+            this.CellsFieldInForm.AllowUserToResizeColumns = false;
+            this.CellsFieldInForm.AllowUserToResizeRows = false;
+            this.CellsFieldInForm.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.CellsFieldInForm.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.CellsFieldInForm.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.CellsFieldInForm.ColumnHeadersVisible = false;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.CellsFieldInForm.DefaultCellStyle = dataGridViewCellStyle5;
+            this.CellsFieldInForm.Location = new System.Drawing.Point(295, 69);
+            this.CellsFieldInForm.MultiSelect = false;
+            this.CellsFieldInForm.Name = "CellsFieldInForm";
+            this.CellsFieldInForm.ReadOnly = true;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.CellsFieldInForm.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.CellsFieldInForm.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.CellsFieldInForm.RowTemplate.Height = 24;
+            this.CellsFieldInForm.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.CellsFieldInForm.Size = new System.Drawing.Size(867, 579);
+            this.CellsFieldInForm.TabIndex = 3;
+            this.CellsFieldInForm.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.CellsFieldInForm_CellMouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1174, 698);
+            this.Controls.Add(this.CellsFieldInForm);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.BtnStart);
+            this.Controls.Add(this.Ed1);
+            this.Controls.Add(this.Lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Ed1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CellsFieldInForm)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lbl1;
+        private System.Windows.Forms.NumericUpDown Ed1;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.DataGridView CellsFieldInForm;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ vildanov_lab3/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vildanov_lab3
+{
+    public partial class Form1 : Form
+    {
+        private int RowCount = 0;
+        private int CellsInRowCount = 20;
+        private bool CellChangeColor = true;
+        private char[] Rule = new char[8];
+        private String[] Patterns = new string[] { "111", "110", "101", "100", "011", "010", "001", "000" };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < CellsInRowCount; i++)
+            {
+                CellsFieldInForm.Columns.Add("", "");
+            }
+
+            RowCount = 1;
+            CellsFieldInForm.Rows.Add();
+            CellsFieldInForm.Rows[0].HeaderCell.Value = RowCount.ToString();
+
+            SetRule(Convert.ToString((int)Ed1.Value, 2));
+
+        }
+
+        private void SetRule(string input)
+        {
+            int len = input.Length;
+            while (len < 8)
+            {
+                input = "0" + input;
+                len = input.Length;
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                Rule[i] = input[i];
+            }
+        }
+        private void edRule_ValueChanged(object sender, EventArgs e)
+        {
+            SetRule(Convert.ToString((int)Ed1.Value, 2));
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            CellChangeColor = false;
+            timer1.Start();
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            char[] PreviousRow = new char[CellsInRowCount];
+            char[] NewRow = new char[CellsInRowCount];
+
+            for (int i = 0; i < CellsInRowCount; i++)
+            {
+                if (CellsFieldInForm[i, RowCount - 1].Style.BackColor == Color.Green) PreviousRow[i] = '1';
+                else PreviousRow[i] = '0';
+            }
+
+            for (int i = 0; i < CellsInRowCount; i++)
+            {
+                string Pattern = "";
+                int LeftCell = i - 1;
+                int RightCell = i + 1;
+                var Builder = new StringBuilder();
+
+                if (i == 0) LeftCell = CellsInRowCount - 1;
+                if (i == CellsInRowCount - 1) RightCell = 0;
+
+                Builder.Append(PreviousRow[LeftCell]);
+                Builder.Append(PreviousRow[i]);
+                Builder.Append(PreviousRow[RightCell]);
+
+                Pattern = Builder.ToString();
+
+                int index = Array.IndexOf(Patterns, Pattern);
+                NewRow[i] = Rule[index];
+
+            }
+
+            RowCount = RowCount + 1;
+            CellsFieldInForm.Rows.Add();
+            CellsFieldInForm.Rows[RowCount - 1].HeaderCell.Value = RowCount.ToString();
+
+            for (int i = 0; i < CellsInRowCount; i++)
+            {
+                if (NewRow[i] == '1') CellsFieldInForm[i, RowCount - 1].Style.BackColor = Color.Green;
+                else CellsFieldInForm[i, RowCount - 1].Style.BackColor = Color.White;
+            }
+
+        }
+
+        private void CellsFieldInForm_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (CellChangeColor)
+            {
+                if (CellsFieldInForm[e.ColumnIndex, e.RowIndex].Style.BackColor == Color.White)
+                {
+                    CellsFieldInForm[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.Green;
+                    CellsFieldInForm.ClearSelection();
+                }
+                else
+                {
+                    CellsFieldInForm[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.White;
+                    CellsFieldInForm.ClearSelection();
+                }
+            }
+        }
+    }
+}
